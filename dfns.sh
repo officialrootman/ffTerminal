6e2e7ab19368e57d039025ee8e6e3029052838d5
officialrootman <muhammetcanaltan41@gmail.com>
2025-04-09 22:47:35 +0300

    Update dfns.sh

--- dfns.sh
+++ dfns.sh
@@ -8,8 +8,8 @@ LOG_FILE="honeypot_logs.txt"
 
 # Netcat ile sahte bir HTTP sunucusu oluştur
 while true; do
-  # Bağlantıyı dinle
-  echo -e "HTTP/1.1 200 OK\n\nSahte Veri: Honeypot çalışıyor" | nc -l -p 8080 -q 1 >> $LOG_FILE
+  # Bağlantıyı dinle ve yanıt ver
+  echo -e "HTTP/1.1 200 OK\n\nSahte Veri: Honeypot çalışıyor" | nc -l -p 8080 >> $LOG_FILE
 
   # Gelen bağlantıyı logla
   echo "$(date) - Saldırgan bağlandı!" >> $LOG_FILE
