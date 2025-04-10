022241d6ec95ce099657086cce4514da7300acf7
officialrootman <muhammetcanaltan41@gmail.com>
2025-04-10 19:18:02 +0300

    Update dfns.py

--- dfns.py
+++ dfns.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 import redis
 
 # Redis: Rate Limiting için kullanılır
-redis_client = redis.StrictRedis(host='localhost', port=8080, db=0)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
 
